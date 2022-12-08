0bc5f31ee3a39c17fa3d8030c402c01819415a19
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-08 01:26:52 -0800

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Retern: Return(0)
+  */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
