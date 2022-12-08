496cc7b8359244864297f5cbf5bc8fd9f26e80ef
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-08 01:49:43 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Print "with proper grammar, but the outcome is a piece of art"
+  *
+  *Return: Alweys 0 to be success
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
