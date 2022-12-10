a3b51db43d44fa7a44de7818a078eb82ad55d7e8
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-10 01:41:27 -0800

    5-print_numbers.c5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main- Entry point
+  * @void: Null value
+  *
+  * Description: Print numbers from 0 to 9
+  * Return: Zero value
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	prints("\n");
+	return (0);
+}
