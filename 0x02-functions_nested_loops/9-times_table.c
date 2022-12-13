e72ee76de3327a183d8f0349327ad4fefaf81657
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-13 11:23:16 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,18 @@ void times_table(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
+		_putchar('0');
+
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			res = num1 * num2;
 			_putchar(',');
 			_putchar(' ');
+			res = num1 * num2;
 
 			if (res <= 9)
-			{
+			
 				_putchar(' ');
-			}
+			
 			else
 				_putchar((res / 10) + '0');
 			_putchar((res % 10) + '0');
