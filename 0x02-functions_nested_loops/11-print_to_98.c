14fc66ac656e5cdafa3ae330b7d045113b24e631
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-13 11:28:31 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,7 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			printf("%d\n", 98);
 		}
+	}
 		else
 		{
 			for (n = n; n > 98; n--)
@@ -22,5 +23,5 @@ void print_to_98(int n)
 			printf("%d\n", 98);
 			}
 		}
-	}
+
 }
