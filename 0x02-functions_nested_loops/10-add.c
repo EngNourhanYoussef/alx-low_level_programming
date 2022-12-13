9a92edb419210c70aa28ce0c7eb902359b7c085c
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-13 01:41:24 -0800

    #include holberton.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two Numbers.
+  * @num1: The first number to be added.
+  * @num2: The second number to be added.
+  *
+  * Return: The result of the addition.
+  */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
