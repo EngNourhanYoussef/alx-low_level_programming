b9ac5fcf9196acbd6e364990041a64f10e80cadd
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-15 01:46:50 -0800

    5-more_numbers.c exit q exit() cd ..

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+  * more_numbers - Print 10 times the numbers since 0 up to 14
+  * Description: Prints the numbers 10 times from 0 to 14
+  * Return: the number from 0 to 14 10 times
+  */
+void more_numbers(void)
+{
+	int c, n;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
