5b688ff97fe171ca2e509218e3d6b61aea5b1688
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-19 08:45:51 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,16 +5,13 @@
   */
 void print_rev(char *s)
 {
-	int i;
+	int len = 0, index;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
 	_putchar('\n');
 }
