8db6636597ecddfa6688b57345e753584b612586
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-19 22:58:16 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * puts2 - Prints out every other char of the string.
+  * @str: input string to print.
+  */
+void puts2(char *str)
+{
+	int inx;
+
+	while (str[inx] != '\0')
+	{
+		if (inx % 2 == 0)
+			_putchar(str[inx]);
+		idx++;
+	}
+	_putchar('\n');
+}
