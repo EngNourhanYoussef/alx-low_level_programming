5da3092ee60bf57620e9b17841a623fb4ff76fba
Nourhan Youssef <Nourhan201717@yahoo.com>
2022-12-21 00:22:13 -0800

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * reverse_array - reverse array
+  * @a:array
+  * @n:integer
+  * Return:void
+  */
+void reverse_array(int *a, int n)
+{
+	int x, temp;
+
+	for (x = 0; (x < (n - 1) / 2); x++)
+	{
+		temp = a[x];
+		a[x] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
