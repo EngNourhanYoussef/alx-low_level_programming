64e740879c615952f6b4d493724ddcdba33b620d
Nourhan Youssef <Nourhan201717@yahoo.com>
2023-01-01 23:13:37 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
   * _mamset - fill memory with constant byte
   * @s:char
   * @b:char
-  * @n:unsigned int 
+  * @n:unsigned int
   * Return:char
   */
 char *_memset(char *s, char b, unsigned int n)
