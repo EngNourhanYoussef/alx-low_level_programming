642733d16a5fca7731620bd7d713a1dd1983e8a8
Nourhan Youssef <Nourhan201717@yahoo.com>
2023-01-18 23:14:43 -0800

    solved

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,5 +4,18 @@
   * sum_them_all - sum all the args
   * @n : the num. passed to fn.
   * @...: unkown num. of args
+  * Return: sum of all num.
   */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index, sum = 0;
 
+	va_start(nums, n);
+	for (index = 0; index < n; index++)
+	{
+		sum += va_arg(nums, int);
+	}
+	va_end(nums);
+	return (sum);
+}
