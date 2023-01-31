ac9d07556d61c37a28167b86b21c7a226d7d93f2
Nourhan Youssef <Nourhan201717@yahoo.com>
2023-01-31 08:47:21 -0800

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,7 @@
   * @n: value of the inserted node
   * Return: pointer to head of list
   */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *current;
 
@@ -18,13 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		return (NULL);
 	}
 	new->n;
-	if (index == 0)
+	if (idx == 0)
 	{
 		new->next = current;
 		*head = new;
 		return (*head);
 	}
-	while (index > 1)
+	while (idx > 1)
 	{
 		current = current->next;
 		index--;
