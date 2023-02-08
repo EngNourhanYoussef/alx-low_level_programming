8d6011062c294facb90ed817613f5f44c45fcf37
Nourhan Youssef <Nourhan201717@yahoo.com>
2023-02-08 03:29:24 -0800

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,6 +16,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
 	rd = read(fd, buff, letters);
 	if (rd == -1)
 	{
